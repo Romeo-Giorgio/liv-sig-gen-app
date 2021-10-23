c49c0eb4ddab6ee172b6fc60183b8d398a218a6d
Roméo <giorgio.romeo95@gmail.com>
2021-10-23 15:03:45 +0200

    [Map] Add Google Map API link and create GMap component

---
+++ src/3-organisms/GMap/GMap.stories.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import GMap from "./GMap";
+
+//********** Stories **********//
+export default {
+    component: GMap,
+    title: "3-organisms/GMap",
+    parameters: {
+      docs: {
+        description: {
+          component: ``,
+        },
+      },
+    },
+  };
+  
+  export const DefaultStory = () => {
+  
+    return (
+      <GMap
+        
+      />
+    );
+  };
+  DefaultStory.storyName = "Default";
