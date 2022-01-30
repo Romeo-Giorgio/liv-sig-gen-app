f691e83cde8a088d46e80f2b87a4ca675acc13b0
Roméo <giorgio.romeo95@gmail.com>
2022-01-30 18:00:37 +0100

    [Menu] Add Main Menu Panel

--- src/3-organisms/MainMenu/MainMenu.tsx
+++ src/3-organisms/MainMenu/MainMenu.tsx
@@ -1,28 +1,44 @@
+//********** Imports **********//
 import { Props } from "./types";
 import {
     AppBar , Toolbar,  Divider, IconButton 
   } from "@material-ui/core";
-  import { Layers ,Menu } from "@material-ui/icons";
-const MainMenu = (_: Props) => {
+import { People, Timeline ,Room } from "@material-ui/icons";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+
+//********** Component **********//
+const MainMenu = (props: Props) => {
+    const {onIntersectionsButtonClick, onSignalersButtonClick, onTracksButtonClick} = props;
     return (
         <AppBar style={{
             borderRadius: "50px",
-            width: "318px",
-            backgroundColor: "Background",
+            width: "200px",
+            backgroundColor: blueGrey[100],
             top:"4px",
             left: "calc(50% - 200px)",}}
             position="static">
             <Toolbar>
-                <IconButton>
-                    <Menu />
+                <IconButton
+                onClick={onTracksButtonClick}>
+                    <Timeline />
                 </IconButton>
                 <Divider
                     orientation="vertical"
                     variant="fullWidth"
                     flexItem
                 />
-                <IconButton>
-                    <Layers />
+                <IconButton
+                onClick={onIntersectionsButtonClick}>
+                    <Room />
+                </IconButton>
+                <Divider
+                    orientation="vertical"
+                    variant="fullWidth"
+                    flexItem
+                />
+                <IconButton
+                onClick={onSignalersButtonClick}>
+                    <People />
                 </IconButton>
             </Toolbar>
         </AppBar>
